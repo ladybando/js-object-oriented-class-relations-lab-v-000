1c819d7c75daaef11d2612d920fe4b9df2aebd9b
Ayana Bando <ayanaflax@yahoo.com>
2019-01-01 19:28:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-let store = {drivers: [], passengers: [], trips: []}
+let store = {drivers: [], passengers: [], trips: []};
 
-let driverId = 0
+let driverId = 0;
 
-let passengerId = 0
+let passengerId = 0;
 
-let tripId = 0
+let tripId = 0;
 
 class Driver {
   constructor(name){
