cad20769bb33ff39834a6b75615e4e713e3d6fa8
Ayana Bando <ayanaflax@yahoo.com>
2019-01-01 20:11:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ class Driver {
 			function(trip) {
 				return trip.driverId === this.id;
 			}.bind(this)
-		);
+		)
 	}
 
 	passengers () {
@@ -27,30 +27,24 @@ class Driver {
 			function(trip) {
 				return trip.passenger();
 			}
-		);
+		)
 	}
  }
 
-class Passenger {
-  constructor(name){
-    this.id = ++passengerId;
-    this.name = name;
+ class Passenger {
+ 	constructor(name) {
+		this.id = ++passengerId;
+		this.name = name;
 
- 
-    // insert in the item to the store
-    store.passengers.push(this);
-  }
-  
-      setTrip(trip) {
-        this.tripId = trip.id;
-  }
-  
-  	trips () {
+		store.passengers.push(this);
+ 	}
+
+	trips () {
 		return store.trips.filter(
 			function(trip) {
 				return trip.passengerId === this.id;
 			}.bind(this)
-		);
+		)
 	}
 
 	drivers () {
@@ -58,26 +52,24 @@ class Passenger {
 			function(trip) {
 				return trip.driver();
 			}
-		);
+		)
 	}
-  
 }
 
-class Trip {
-    constructor(name){
-    this.id = ++tripId;
-    this.name = name;
- 
-    // insert in the item to the store
-    store.trips.push(this);
-  }
-  
-driver () {
+ class Trip {
+ 	constructor(driver, passenger) {
+		this.id = ++tripId;
+		this.driverId = driver.id;
+		this.passengerId = passenger.id;
+		store.trips.push(this);
+ 	}
+
+	driver () {
 		return store.drivers.find(
 			function(driver) {
 				return driver.id === this.driverId;
 			}.bind(this)
-		);
+		)
 	}
 
 	passenger () {
@@ -85,7 +77,6 @@ driver () {
 			function(passenger) {
 				return passenger.id === this.passengerId;
 			}.bind(this)
-		);
- 
-	}	
+		)
+	}
 }
